b343bcababf19c222f0699872bde58baf499fd0e
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 18:56:56 +0600

    CategoryForm from added

---
+++ category/forms.py
@@ -0,0 +1,10 @@
+# category/forms.py
+from django import forms
+
+from .models import TaskCategory
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = TaskCategory
+        fields = "__all__"
