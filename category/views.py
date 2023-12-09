1a49d6934e39de4fd1c2dc25915c1eb52a23f345
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 18:57:45 +0600

    views added

--- category/views.py
+++ category/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from task.models import TaskCategory
+
+from .forms import CategoryForm
+
+
+def show_categories(request):
+    tasks = TaskCategory.objects.all()
+    return render(request, "show_tasks.html", {"tasks": tasks})
+
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("show_categories")
+    else:
+        form = CategoryForm()
+    return render(request, "add_category.html", {"form": form})
