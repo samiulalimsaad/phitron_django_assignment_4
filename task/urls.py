c26553091fddf61023614b1df4e42a4158bb44bb
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 19:20:27 +0600

    routes added

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import add_task, show_tasks
+from .views import add_task, delete_task, edit_task, show_tasks
 
 urlpatterns = [
     path("show-tasks/", show_tasks, name="show_tasks"),
     path("add-task/", add_task, name="add_task"),
+    path("edit-task/<int:task_id>/", edit_task, name="edit_task"),
+    path("delete-task/<int:task_id>/", delete_task, name="delete_task"),
 ]
