394c7c1bff5ddfa9b2919359dfc3150e8c447b7f
samiulalimsaad <samiulalimsaad@gmail.com>
2023-12-09 18:59:31 +0600

    task controller added

--- task/views.py
+++ task/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import TaskForm
+from .models import TaskModel
+
+
+def show_tasks(request):
+    tasks = TaskModel.objects.all()
+    return render(request, "show_tasks.html", {"tasks": tasks})
+
+
+def add_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("show_tasks")
+    else:
+        form = TaskForm()
+    return render(request, "add_task.html", {"form": form})
